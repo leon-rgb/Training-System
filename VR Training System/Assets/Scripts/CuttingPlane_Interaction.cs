2c364589eb299d06514182320a7e3c25aeffc756
leon-rgb <56979997+leon-rgb@users.noreply.github.com>
2021-11-11 17:57:18 +0100

    Added realistic saw and holoSaw that shows where to place the Saw

--- VR Training System/Assets/Scripts/CuttingPlane_Interaction.cs
+++ VR Training System/Assets/Scripts/CuttingPlane_Interaction.cs
@@ -40,10 +40,40 @@ public class CuttingPlane_Interaction : MonoBehaviour
     [SerializeField]
     private float AngleZ;
 
+
+    public GameObject SawHolo;
+    public Material SawRed;
+    public Material SawGreen;
+    private Vector3 SawHoloPos;
+    private float SawHoloPosX;
+    private float SawHoloPosY;
+    private float SawHoloPosZ;
+    private Vector3 SawHoloRot;
+    private Vector3 Pos;
+    private float PosX;
+    private float PosY;
+    private float PosZ;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SawHoloPos = SawHolo.transform.position;
+        SawHoloPosX = SawHoloPos.x;
+        SawHoloPosY = SawHoloPos.y;
+        SawHoloPosZ = SawHoloPos.z;
+
+        SawHoloRot = SawHolo.transform.eulerAngles;
+
+        // we need the "normal" euler Angles (not local)
+        Debug.Log("euler: " + SawHoloRot);
+        SawHoloRot = SawHolo.transform.localEulerAngles;
+        /*
+        Debug.Log("localeuler: " + SawHoloRot);
+        Debug.Log("euler: " + this.transform.eulerAngles);
+        Debug.Log("localeuler: " + this.transform.localEulerAngles);
+        */
+        Debug.Log("pos: " +Pos);
+        Debug.Log("posHolo: " + SawHoloPos);
     }
 
     // Update is called once per frame
@@ -54,12 +84,35 @@ public class CuttingPlane_Interaction : MonoBehaviour
         //ThisCollider = this.GetComponent<Collider>();
 
         CuttingPlaneCollider = CuttingPlane.GetComponent<Collider>();
-        
+
+        Pos = this.transform.position;
+        PosX = Pos.x;
+        PosY = Pos.y;
+        PosZ = Pos.z;
+         
+        bool xPosIsOk = SawHoloPosX+0.025f > PosX && SawHoloPosX-0.025f <PosX;
+        bool yPosIsOk = SawHoloPosY + 0.025f > PosY && SawHoloPosY - 0.025f < PosY; 
+        bool zPosIsOk = SawHoloPosZ + 0.025f > PosZ && SawHoloPosZ - 0.025f < PosZ;
+
+        if (xPosIsOk && yPosIsOk && zPosIsOk){
+            ChangeHoloSawColor(SawGreen);
+        }
+        else
+        {
+            ChangeHoloSawColor(SawRed);
+        }
+
+
+        /**
+         * TODO: auch Winkel miteinabauen!
+         */
+         
     }
 
 
     private void OnTriggerEnter(Collider other)
     {
+        /*
         if (other == CuttingPlaneCollider)
         {
             CuttingPlane.GetComponent<MeshRenderer>().material = CorrectInteractionMaterial;
@@ -69,10 +122,22 @@ public class CuttingPlane_Interaction : MonoBehaviour
             OnTriggerEnterEulerAngleZ = this.transform.localEulerAngles.z;
             Debug.Log("TRIGGER ENTERED");
         }
+        */
+       
+        if(other.tag == "SawHolo")
+        {
+            Debug.Log("SAW HOLO TRIGGER ENTERED");
+            foreach (Transform child in other.transform)
+            {
+                Debug.Log("this is a child: " + child.name);
+                child.GetComponent<MeshRenderer>().enabled = true;
+            }
+        }
     }
 
     private void OnTriggerStay(Collider other)
     {
+        /*
         if (other == CuttingPlaneCollider)
         {
             OnTriggerStayPosition = this.transform.position;
@@ -91,6 +156,51 @@ public class CuttingPlane_Interaction : MonoBehaviour
                 CuttingPlane.GetComponent<MeshRenderer>().material = DefaultMaterial;
             }
         }
+        */
+
     }
 
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "SawHolo")
+        {
+            foreach (Transform child in other.transform)
+            {
+                Debug.Log("this is a child: " + child.name);
+                child.GetComponent<MeshRenderer>().enabled = false;
+            }
+        }
+    }
+
+    /*
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("COLLISION WITH: " + collision.gameObject.name);
+        if(collision.collider.tag == "CuttingPlane")
+        {
+            collision.rigidbody.isKinematic = true;
+            Debug.Log("CUTTINGPLANE HIT");
+            CuttingPlane.GetComponent<MeshRenderer>().material = CorrectInteractionMaterial;
+        }
+
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        //Debug.Log("COLLISION WITH: " + collision.gameObject.name);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        collision.rigidbody.isKinematic = false;
+    }
+    */
+
+    private void ChangeHoloSawColor(Material material)
+    {
+        foreach (Transform child in SawHolo.transform)
+        {
+            child.GetComponent<MeshRenderer>().material = material;
+        }
+    }
 }
